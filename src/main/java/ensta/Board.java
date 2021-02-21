558ca6d043dddadd63b932f956b76adc1e75a3ef
Nico Sigal <nicolassigal@Mac-Nico.local>
2021-02-21 09:53:49 +0100

    Exercice 1

--- src/main/java/ensta/Board.java
+++ src/main/java/ensta/Board.java
@@ -25,123 +25,123 @@ public class Board {
 	
 	public void setName(String name) {this.name = name; }
 	
-	public Character[][] getMyGrid() {return ship; }
+	public Character[][] getShip() {return ship; }
 	
-	public Boolean[][] getEnemyGrid() {	return hits; }
+	public Boolean[][] getHits() {	return hits; }
 	
 	public int getSize() { return this.size; }
 
-	public void saut_ligne(int n){
+	public void sautLigne(int n){
 		for (int i = 0 ; i < n ; i++){
 			System.out.println(" ");
 		}
 	}
 
-	public void printspaces(int n){
+	public void printSpaces(int n){
 		for (int i = 0 ; i < n ; i++){
 			System.out.print(" ");
 		}
 	}
 
-	public void centrer_nom(){
-		saut_ligne(1);
-		printspaces(2 * getSize() + 6 - this.name.length()/2 + 2);	// centrer le nom
+	public void centrerNom(){
+		sautLigne(1);
+		printSpaces(2 * getSize() + 6 - this.name.length()/2 + 2);	// centrer le nom
 		System.out.println(this.name);
-		saut_ligne(1);
+		sautLigne(1);
 	}
 	
-	public void affichage_init(){
-		centrer_nom();
-		printspaces(getSize() + 3);
+	public void affichageInit(){
+		centrerNom();
+		printSpaces(getSize() + 3);
 		System.out.print("SHIPS");
-		printspaces(2 * getSize());
+		printSpaces(2 * getSize());
 		System.out.print("HITS");
-		saut_ligne(2);		
-		affichage_pointillés();
+		sautLigne(2);		
+		affichagePointillés();
 	}
 	
-	public void affichage_lettre(){
+	public void affichageLettre(){
 		char ch = 'A';
 		for (int i = 0 ; i < getSize() ; i ++){
 			System.out.print(ch);
 			ch += 1;
-			printspaces(1);
+			printSpaces(1);
 		}
 	}
 	
-	public void affichage_lettres_global(){
-		printspaces(5);
-		affichage_lettre();
-		printspaces(6);
-		affichage_lettre();
-		saut_ligne(1);
+	public void affichageLettresGlobal(){
+		printSpaces(5);
+		affichageLettre();
+		printSpaces(6);
+		affichageLettre();
+		sautLigne(1);
 	}
 	
-	public void affichage_pointillés(){
+	public void affichagePointillés(){
 		for (int k = 0 ; k < 4 * getSize() + 14 ; k ++){
 			System.out.print("-");
 		}
-		saut_ligne(1);
+		sautLigne(1);
 	}
 	
-	public void affichage_numéros_début_lignes(int i){
+	public void affichageNumérosDébutLignes(int i){
 		if (i < 9) {
-			printspaces(1);
+			printSpaces(1);
 		}
 		System.out.print(i+1);
-		printspaces(1);
+		printSpaces(1);
 		System.out.print("|");
-		printspaces(1);
+		printSpaces(1);
 	}
 	
-	public void affichage_ships(int i, int j){
+	public void affichageShips(int i, int j){
 		System.out.print(this.ship[i][j]);
-		printspaces(1);
+		printSpaces(1);
 	}
 	
 	public void affichage_central(){
-		printspaces(1);
+		printSpaces(1);
 		System.out.print(" || ");
-		printspaces(1);
+		printSpaces(1);
 	}
 	
-	public void affichage_hits(int i, int j){
+	public void affichageHits(int i, int j){
 		if (this.hits[i][j]) {
 			System.out.print("x");
-			printspaces(1);
+			printSpaces(1);
 		}
 		else {
 			System.out.print(".");
-			printspaces(1);
+			printSpaces(1);
 		}
 	}
 	
-	public void affichage_numéros_fin_lignes(int i){
+	public void affichageNumérosFinLignes(int i){
 		System.out.print("|");
 		if (i < 9) {
-			printspaces(1);
+			printSpaces(1);
 		}
 		System.out.print(i+1);
-		printspaces(1);
-		saut_ligne(1);
+		printSpaces(1);
+		sautLigne(1);
 	}
 	
-	public void affichage_grilles(){
+	public void affichageGrilles(){
 			for (int i = 0 ; i < getSize() ; i++){
-				affichage_numéros_début_lignes(i);
-				for (int j = 0; j < getSize() ; j++) { affichage_ships(i,j); }
+				affichageNumérosDébutLignes(i);
+				for (int j = 0; j < getSize() ; j++) { affichageShips(i,j); }
 				affichage_central();
-				for (int j = 0; j < getSize() ; j++) { affichage_hits(i,j);}
-				affichage_numéros_fin_lignes(i);
+				for (int j = 0; j < getSize() ; j++) { affichageHits(i,j);}
+				affichageNumérosFinLignes(i);
 			}
 	}
 
 	public void print() {
-		affichage_init();
-		affichage_lettres_global();
-		affichage_pointillés();
-		affichage_grilles();
-		affichage_pointillés();
-	saut_ligne(2);
+		affichageInit();
+		affichageLettresGlobal();
+		affichagePointillés();
+		affichageGrilles();
+		affichagePointillés();
+	sautLigne(2);
 	}
 }
